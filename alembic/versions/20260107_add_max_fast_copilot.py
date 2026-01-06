e99a6cf99164570f415fc06466865c95c50b27a0
jomiferse <jomiferse@gmail.com>
2026-01-06 11:48:45 +0100

    Shorten alembic revision id

--- alembic/versions/20260107_add_max_fast_copilot.py
+++ alembic/versions/20260107_add_max_fast_copilot.py
@@ -1,6 +1,6 @@
 """Add max_fast_copilot_per_day to plans.
 
-Revision ID: 20260107_add_max_fast_copilot_per_day
+Revision ID: 20260107_add_max_fast_copilot
 Revises: 20260106_add_alert_best_ask
 Create Date: 2026-01-07 00:00:00.000000
 """
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = "20260107_add_max_fast_copilot_per_day"
+revision = "20260107_add_max_fast_copilot"
 down_revision = "20260106_add_alert_best_ask"
 branch_labels = None
 depends_on = None
